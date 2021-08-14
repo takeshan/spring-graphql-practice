ec70239550c484b33107701843e69a9a302e2917
takeshan <awdrgyjilp10098@gmail.com>
2021-08-14 15:13:42 +0900

    spring initializer generated

---
+++ src/main/java/com/takeshan/springgraphqlpractice/SpringGraphqlPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.takeshan.springgraphqlpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takeshan/springgraphqlpractice/SpringGraphqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takeshan.springgraphqlpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
